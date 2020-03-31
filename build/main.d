cc2bb14a349447bfa0835a7780d65e0b0ede2b9b
AndreYin <andre@asteristic.com>
2020-03-31 14:13:09 -0300

    First Commit

---
+++ build/main.d
@@ -0,0 +1,412 @@
+main.o: C:/Users/andre/libtesla-master/example/source/main.cpp \
+ c:\users\andre\libtesla-master\include\tesla.hpp \
+ E:/devkitPro/libnx/include/switch.h \
+ E:/devkitPro/libnx/include/switch/types.h \
+ E:/devkitPro/libnx/include/switch/result.h \
+ E:/devkitPro/libnx/include/switch/nro.h \
+ E:/devkitPro/libnx/include/switch/nacp.h \
+ E:/devkitPro/libnx/include/switch/arm/tls.h \
+ E:/devkitPro/libnx/include/switch/arm/cache.h \
+ E:/devkitPro/libnx/include/switch/arm/atomics.h \
+ E:/devkitPro/libnx/include/switch/arm/counter.h \
+ E:/devkitPro/libnx/include/switch/kernel/svc.h \
+ e:\devkitpro\libnx\include\switch\arm\thread_context.h \
+ E:/devkitPro/libnx/include/switch/kernel/wait.h \
+ E:/devkitPro/libnx/include/switch/kernel/mutex.h \
+ E:/devkitPro/libnx/include/switch/kernel/tmem.h \
+ E:/devkitPro/libnx/include/switch/kernel/shmem.h \
+ E:/devkitPro/libnx/include/switch/kernel/event.h \
+ E:/devkitPro/libnx/include/switch/kernel/uevent.h \
+ E:/devkitPro/libnx/include/switch/kernel/utimer.h \
+ E:/devkitPro/libnx/include/switch/kernel/rwlock.h \
+ e:\devkitpro\libnx\include\switch\kernel\condvar.h \
+ E:/devkitPro/libnx/include/switch/kernel/thread.h \
+ E:/devkitPro/libnx/include/switch/kernel/semaphore.h \
+ E:/devkitPro/libnx/include/switch/kernel/virtmem.h \
+ E:/devkitPro/libnx/include/switch/kernel/detect.h \
+ E:/devkitPro/libnx/include/switch/kernel/random.h \
+ E:/devkitPro/libnx/include/switch/kernel/jit.h \
+ E:/devkitPro/libnx/include/switch/kernel/ipc.h \
+ E:/devkitPro/libnx/include/switch/kernel/barrier.h \
+ E:/devkitPro/libnx/include/switch/sf/hipc.h \
+ E:/devkitPro/libnx/include/switch/sf/cmif.h \
+ E:/devkitPro/libnx/include/switch/sf/service.h \
+ E:/devkitPro/libnx/include/switch/sf/sessionmgr.h \
+ E:/devkitPro/libnx/include/switch/services/sm.h \
+ E:/devkitPro/libnx/include/switch/services/smm.h \
+ E:/devkitPro/libnx/include/switch/services/fs.h \
+ e:\devkitpro\libnx\include\switch\services\ncm_types.h \
+ e:\devkitpro\libnx\include\switch\crypto\sha256.h \
+ e:\devkitpro\libnx\include\switch\services\acc.h \
+ E:/devkitPro/libnx/include/switch/services/fsldr.h \
+ E:/devkitPro/libnx/include/switch/services/fspr.h \
+ E:/devkitPro/libnx/include/switch/services/apm.h \
+ E:/devkitPro/libnx/include/switch/services/applet.h \
+ e:\devkitpro\libnx\include\switch\services\pdm.h \
+ e:\devkitpro\libnx\include\switch\services\caps.h \
+ e:\devkitpro\libnx\include\switch\services\pm.h \
+ e:\devkitpro\libnx\include\switch\services\set.h \
+ e:\devkitpro\libnx\include\switch\services\time.h \
+ E:/devkitPro/libnx/include/switch/services/async.h \
+ e:\devkitpro\libnx\include\switch\applets\error.h \
+ E:/devkitPro/libnx/include/switch/services/audin.h \
+ e:\devkitpro\libnx\include\switch\audio\audio.h \
+ E:/devkitPro/libnx/include/switch/services/audout.h \
+ E:/devkitPro/libnx/include/switch/services/audren.h \
+ E:/devkitPro/libnx/include/switch/services/auddev.h \
+ E:/devkitPro/libnx/include/switch/services/hwopus.h \
+ E:/devkitPro/libnx/include/switch/services/csrng.h \
+ E:/devkitPro/libnx/include/switch/services/lbl.h \
+ E:/devkitPro/libnx/include/switch/services/i2c.h \
+ E:/devkitPro/libnx/include/switch/services/gpio.h \
+ E:/devkitPro/libnx/include/switch/services/bpc.h \
+ E:/devkitPro/libnx/include/switch/services/pcv.h \
+ E:/devkitPro/libnx/include/switch/services/clkrst.h \
+ E:/devkitPro/libnx/include/switch/services/psm.h \
+ E:/devkitPro/libnx/include/switch/services/spsm.h \
+ E:/devkitPro/libnx/include/switch/services/fatal.h \
+ E:/devkitPro/libnx/include/switch/services/usb.h \
+ E:/devkitPro/libnx/include/switch/services/usbds.h \
+ E:/devkitPro/libnx/include/switch/services/usbhs.h \
+ E:/devkitPro/libnx/include/switch/services/hid.h \
+ E:/devkitPro/libnx/include/switch/services/hiddbg.h \
+ E:/devkitPro/libnx/include/switch/services/hidsys.h \
+ E:/devkitPro/libnx/include/switch/services/irs.h \
+ E:/devkitPro/libnx/include/switch/services/pl.h \
+ E:/devkitPro/libnx/include/switch/services/vi.h \
+ E:/devkitPro/libnx/include/switch/services/nv.h \
+ E:/devkitPro/libnx/include/switch/services/nifm.h \
+ E:/devkitPro/libnx/include/switch/services/ns.h \
+ E:/devkitPro/libnx/include/switch/services/ldr.h \
+ E:/devkitPro/libnx/include/switch/services/ro.h \
+ E:/devkitPro/libnx/include/switch/services/ts.h \
+ E:/devkitPro/libnx/include/switch/services/lr.h \
+ E:/devkitPro/libnx/include/switch/services/spl.h \
+ E:/devkitPro/libnx/include/switch/services/ncm.h \
+ E:/devkitPro/libnx/include/switch/services/psc.h \
+ E:/devkitPro/libnx/include/switch/services/capsu.h \
+ E:/devkitPro/libnx/include/switch/services/capssc.h \
+ E:/devkitPro/libnx/include/switch/services/capssu.h \
+ E:/devkitPro/libnx/include/switch/services/nfc.h \
+ E:/devkitPro/libnx/include/switch/services/wlaninf.h \
+ E:/devkitPro/libnx/include/switch/services/pctl.h \
+ E:/devkitPro/libnx/include/switch/services/grc.h \
+ e:\devkitpro\libnx\include\switch\display\native_window.h \
+ e:\devkitpro\libnx\include\switch\nvidia\graphic_buffer.h \
+ e:\devkitpro\libnx\include\switch\display\types.h \
+ e:\devkitpro\libnx\include\switch\nvidia\types.h \
+ e:\devkitpro\libnx\include\switch\display\binder.h \
+ e:\devkitpro\libnx\include\switch\display\buffer_producer.h \
+ e:\devkitpro\libnx\include\switch\nvidia\fence.h \
+ e:\devkitpro\libnx\include\switch\nvidia\ioctl.h \
+ E:/devkitPro/libnx/include/switch/services/friends.h \
+ E:/devkitPro/libnx/include/switch/services/notif.h \
+ E:/devkitPro/libnx/include/switch/display/parcel.h \
+ E:/devkitPro/libnx/include/switch/display/framebuffer.h \
+ e:\devkitpro\libnx\include\switch\nvidia\map.h \
+ E:/devkitPro/libnx/include/switch/nvidia/address_space.h \
+ E:/devkitPro/libnx/include/switch/nvidia/channel.h \
+ E:/devkitPro/libnx/include/switch/nvidia/gpu.h \
+ E:/devkitPro/libnx/include/switch/nvidia/gpu_channel.h \
+ E:/devkitPro/libnx/include/switch/audio/driver.h \
+ E:/devkitPro/libnx/include/switch/applets/libapplet.h \
+ E:/devkitPro/libnx/include/switch/applets/album_la.h \
+ E:/devkitPro/libnx/include/switch/applets/friends_la.h \
+ E:/devkitPro/libnx/include/switch/applets/hid_la.h \
+ E:/devkitPro/libnx/include/switch/applets/pctlauth.h \
+ E:/devkitPro/libnx/include/switch/applets/psel.h \
+ E:/devkitPro/libnx/include/switch/applets/swkbd.h \
+ E:/devkitPro/libnx/include/switch/applets/web.h \
+ E:/devkitPro/libnx/include/switch/runtime/env.h \
+ E:/devkitPro/libnx/include/switch/runtime/hosversion.h \
+ E:/devkitPro/libnx/include/switch/runtime/nxlink.h \
+ E:/devkitPro/libnx/include/switch/runtime/resolver.h \
+ E:/devkitPro/libnx/include/switch/runtime/util/utf.h \
+ E:/devkitPro/libnx/include/switch/runtime/devices/console.h \
+ E:/devkitPro/libnx/include/switch/runtime/devices/usb_comms.h \
+ E:/devkitPro/libnx/include/switch/runtime/devices/fs_dev.h \
+ E:/devkitPro/libnx/include/switch/runtime/devices/romfs_dev.h \
+ E:/devkitPro/libnx/include/switch/runtime/devices/socket.h \
+ E:/devkitPro/libnx/include/switch/crypto/aes.h \
+ E:/devkitPro/libnx/include/switch/crypto/aes_cbc.h \
+ E:/devkitPro/libnx/include/switch/crypto/aes_ctr.h \
+ E:/devkitPro/libnx/include/switch/crypto/aes_xts.h \
+ E:/devkitPro/libnx/include/switch/crypto/cmac.h \
+ E:/devkitPro/libnx/include/switch/crypto/sha1.h \
+ E:/devkitPro/libnx/include/switch/crypto/hmac.h \
+ E:/devkitPro/libnx/include/switch/crypto/crc.h \
+ c:\users\andre\libtesla-master\include\stb_truetype.h
+
+c:\users\andre\libtesla-master\include\tesla.hpp:
+
+E:/devkitPro/libnx/include/switch.h:
+
+E:/devkitPro/libnx/include/switch/types.h:
+
+E:/devkitPro/libnx/include/switch/result.h:
+
+E:/devkitPro/libnx/include/switch/nro.h:
+
+E:/devkitPro/libnx/include/switch/nacp.h:
+
+E:/devkitPro/libnx/include/switch/arm/tls.h:
+
+E:/devkitPro/libnx/include/switch/arm/cache.h:
+
+E:/devkitPro/libnx/include/switch/arm/atomics.h:
+
+E:/devkitPro/libnx/include/switch/arm/counter.h:
+
+E:/devkitPro/libnx/include/switch/kernel/svc.h:
+
+e:\devkitpro\libnx\include\switch\arm\thread_context.h:
+
+E:/devkitPro/libnx/include/switch/kernel/wait.h:
+
+E:/devkitPro/libnx/include/switch/kernel/mutex.h:
+
+E:/devkitPro/libnx/include/switch/kernel/tmem.h:
+
+E:/devkitPro/libnx/include/switch/kernel/shmem.h:
+
+E:/devkitPro/libnx/include/switch/kernel/event.h:
+
+E:/devkitPro/libnx/include/switch/kernel/uevent.h:
+
+E:/devkitPro/libnx/include/switch/kernel/utimer.h:
+
+E:/devkitPro/libnx/include/switch/kernel/rwlock.h:
+
+e:\devkitpro\libnx\include\switch\kernel\condvar.h:
+
+E:/devkitPro/libnx/include/switch/kernel/thread.h:
+
+E:/devkitPro/libnx/include/switch/kernel/semaphore.h:
+
+E:/devkitPro/libnx/include/switch/kernel/virtmem.h:
+
+E:/devkitPro/libnx/include/switch/kernel/detect.h:
+
+E:/devkitPro/libnx/include/switch/kernel/random.h:
+
+E:/devkitPro/libnx/include/switch/kernel/jit.h:
+
+E:/devkitPro/libnx/include/switch/kernel/ipc.h:
+
+E:/devkitPro/libnx/include/switch/kernel/barrier.h:
+
+E:/devkitPro/libnx/include/switch/sf/hipc.h:
+
+E:/devkitPro/libnx/include/switch/sf/cmif.h:
+
+E:/devkitPro/libnx/include/switch/sf/service.h:
+
+E:/devkitPro/libnx/include/switch/sf/sessionmgr.h:
+
+E:/devkitPro/libnx/include/switch/services/sm.h:
+
+E:/devkitPro/libnx/include/switch/services/smm.h:
+
+E:/devkitPro/libnx/include/switch/services/fs.h:
+
+e:\devkitpro\libnx\include\switch\services\ncm_types.h:
+
+e:\devkitpro\libnx\include\switch\crypto\sha256.h:
+
+e:\devkitpro\libnx\include\switch\services\acc.h:
+
+E:/devkitPro/libnx/include/switch/services/fsldr.h:
+
+E:/devkitPro/libnx/include/switch/services/fspr.h:
+
+E:/devkitPro/libnx/include/switch/services/apm.h:
+
+E:/devkitPro/libnx/include/switch/services/applet.h:
+
+e:\devkitpro\libnx\include\switch\services\pdm.h:
+
+e:\devkitpro\libnx\include\switch\services\caps.h:
+
+e:\devkitpro\libnx\include\switch\services\pm.h:
+
+e:\devkitpro\libnx\include\switch\services\set.h:
+
+e:\devkitpro\libnx\include\switch\services\time.h:
+
+E:/devkitPro/libnx/include/switch/services/async.h:
+
+e:\devkitpro\libnx\include\switch\applets\error.h:
+
+E:/devkitPro/libnx/include/switch/services/audin.h:
+
+e:\devkitpro\libnx\include\switch\audio\audio.h:
+
+E:/devkitPro/libnx/include/switch/services/audout.h:
+
+E:/devkitPro/libnx/include/switch/services/audren.h:
+
+E:/devkitPro/libnx/include/switch/services/auddev.h:
+
+E:/devkitPro/libnx/include/switch/services/hwopus.h:
+
+E:/devkitPro/libnx/include/switch/services/csrng.h:
+
+E:/devkitPro/libnx/include/switch/services/lbl.h:
+
+E:/devkitPro/libnx/include/switch/services/i2c.h:
+
+E:/devkitPro/libnx/include/switch/services/gpio.h:
+
+E:/devkitPro/libnx/include/switch/services/bpc.h:
+
+E:/devkitPro/libnx/include/switch/services/pcv.h:
+
+E:/devkitPro/libnx/include/switch/services/clkrst.h:
+
+E:/devkitPro/libnx/include/switch/services/psm.h:
+
+E:/devkitPro/libnx/include/switch/services/spsm.h:
+
+E:/devkitPro/libnx/include/switch/services/fatal.h:
+
+E:/devkitPro/libnx/include/switch/services/usb.h:
+
+E:/devkitPro/libnx/include/switch/services/usbds.h:
+
+E:/devkitPro/libnx/include/switch/services/usbhs.h:
+
+E:/devkitPro/libnx/include/switch/services/hid.h:
+
+E:/devkitPro/libnx/include/switch/services/hiddbg.h:
+
+E:/devkitPro/libnx/include/switch/services/hidsys.h:
+
+E:/devkitPro/libnx/include/switch/services/irs.h:
+
+E:/devkitPro/libnx/include/switch/services/pl.h:
+
+E:/devkitPro/libnx/include/switch/services/vi.h:
+
+E:/devkitPro/libnx/include/switch/services/nv.h:
+
+E:/devkitPro/libnx/include/switch/services/nifm.h:
+
+E:/devkitPro/libnx/include/switch/services/ns.h:
+
+E:/devkitPro/libnx/include/switch/services/ldr.h:
+
+E:/devkitPro/libnx/include/switch/services/ro.h:
+
+E:/devkitPro/libnx/include/switch/services/ts.h:
+
+E:/devkitPro/libnx/include/switch/services/lr.h:
+
+E:/devkitPro/libnx/include/switch/services/spl.h:
+
+E:/devkitPro/libnx/include/switch/services/ncm.h:
+
+E:/devkitPro/libnx/include/switch/services/psc.h:
+
+E:/devkitPro/libnx/include/switch/services/capsu.h:
+
+E:/devkitPro/libnx/include/switch/services/capssc.h:
+
+E:/devkitPro/libnx/include/switch/services/capssu.h:
+
+E:/devkitPro/libnx/include/switch/services/nfc.h:
+
+E:/devkitPro/libnx/include/switch/services/wlaninf.h:
+
+E:/devkitPro/libnx/include/switch/services/pctl.h:
+
+E:/devkitPro/libnx/include/switch/services/grc.h:
+
+e:\devkitpro\libnx\include\switch\display\native_window.h:
+
+e:\devkitpro\libnx\include\switch\nvidia\graphic_buffer.h:
+
+e:\devkitpro\libnx\include\switch\display\types.h:
+
+e:\devkitpro\libnx\include\switch\nvidia\types.h:
+
+e:\devkitpro\libnx\include\switch\display\binder.h:
+
+e:\devkitpro\libnx\include\switch\display\buffer_producer.h:
+
+e:\devkitpro\libnx\include\switch\nvidia\fence.h:
+
+e:\devkitpro\libnx\include\switch\nvidia\ioctl.h:
+
+E:/devkitPro/libnx/include/switch/services/friends.h:
+
+E:/devkitPro/libnx/include/switch/services/notif.h:
+
+E:/devkitPro/libnx/include/switch/display/parcel.h:
+
+E:/devkitPro/libnx/include/switch/display/framebuffer.h:
+
+e:\devkitpro\libnx\include\switch\nvidia\map.h:
+
+E:/devkitPro/libnx/include/switch/nvidia/address_space.h:
+
+E:/devkitPro/libnx/include/switch/nvidia/channel.h:
+
+E:/devkitPro/libnx/include/switch/nvidia/gpu.h:
+
+E:/devkitPro/libnx/include/switch/nvidia/gpu_channel.h:
+
+E:/devkitPro/libnx/include/switch/audio/driver.h:
+
+E:/devkitPro/libnx/include/switch/applets/libapplet.h:
+
+E:/devkitPro/libnx/include/switch/applets/album_la.h:
+
+E:/devkitPro/libnx/include/switch/applets/friends_la.h:
+
+E:/devkitPro/libnx/include/switch/applets/hid_la.h:
+
+E:/devkitPro/libnx/include/switch/applets/pctlauth.h:
+
+E:/devkitPro/libnx/include/switch/applets/psel.h:
+
+E:/devkitPro/libnx/include/switch/applets/swkbd.h:
+
+E:/devkitPro/libnx/include/switch/applets/web.h:
+
+E:/devkitPro/libnx/include/switch/runtime/env.h:
+
+E:/devkitPro/libnx/include/switch/runtime/hosversion.h:
+
+E:/devkitPro/libnx/include/switch/runtime/nxlink.h:
+
+E:/devkitPro/libnx/include/switch/runtime/resolver.h:
+
+E:/devkitPro/libnx/include/switch/runtime/util/utf.h:
+
+E:/devkitPro/libnx/include/switch/runtime/devices/console.h:
+
+E:/devkitPro/libnx/include/switch/runtime/devices/usb_comms.h:
+
+E:/devkitPro/libnx/include/switch/runtime/devices/fs_dev.h:
+
+E:/devkitPro/libnx/include/switch/runtime/devices/romfs_dev.h:
+
+E:/devkitPro/libnx/include/switch/runtime/devices/socket.h:
+
+E:/devkitPro/libnx/include/switch/crypto/aes.h:
+
+E:/devkitPro/libnx/include/switch/crypto/aes_cbc.h:
+
+E:/devkitPro/libnx/include/switch/crypto/aes_ctr.h:
+
+E:/devkitPro/libnx/include/switch/crypto/aes_xts.h:
+
+E:/devkitPro/libnx/include/switch/crypto/cmac.h:
+
+E:/devkitPro/libnx/include/switch/crypto/sha1.h:
+
+E:/devkitPro/libnx/include/switch/crypto/hmac.h:
+
+E:/devkitPro/libnx/include/switch/crypto/crc.h:
+
+c:\users\andre\libtesla-master\include\stb_truetype.h:
